db299c644983093aba67299b791abb6211da52bb
Manav <manavchhabraj7@gmail.com>
2020-05-16 21:17:29 +0530

    Modifying logo Design

--- JS/behave.js
+++ JS/behave.js
@@ -1,3 +1,12 @@
+if($(window).width()<676)
+{
+	$("#UIET-LOGO,#PU-LOGO").addClass("hide");
+}
+else
+{
+	$("#UIET-LOGO,#PU-LOGO").removeClass("hide");
+}
+
 $(window).on('load resize', function()
 {
 	if($(window).width()<676)
