6d8c74c8dd73c7095d31027afb8760ce73db1a1a
Manav <manavchhabraj7@gmail.com>
2020-05-16 21:20:11 +0530

    Modifying logo Designs

--- JS/behave.js
+++ JS/behave.js
@@ -1,12 +1,3 @@
-if($(window).width()<676)
-{
-	$("#UIET-LOGO,#PU-LOGO").addClass("hide");
-}
-else
-{
-	$("#UIET-LOGO,#PU-LOGO").removeClass("hide");
-}
-
 $(window).on('load resize', function()
 {
 	if($(window).width()<676)
