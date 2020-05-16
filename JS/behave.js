0c88132deda75176d225c69edde46aa70da6512a
Manav <manavchhabraj7@gmail.com>
2020-05-16 21:02:35 +0530

    Modifying logo

---
+++ JS/behave.js
@@ -0,0 +1,11 @@
+$(window).on('load resize', function()
+{
+	if($(window).width()<676)
+	{
+		$("#UIET-LOGO,#PU-LOGO").addClass("hide");
+	}
+	else
+	{
+		$("#UIET-LOGO,#PU-LOGO").removeClass("hide");
+	}
+})
