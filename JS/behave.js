6e1fe0b06a9d4bfb0f0ebcd803e634158dd8d86a
Manav <manavchhabraj7@gmail.com>
2020-05-27 21:04:31 +0530

    Dolphy side bar

--- JS/behave.js
+++ JS/behave.js
@@ -1,4 +1,4 @@
 $(".menu-button").on("click",function () 
 {
-	$(".nav-menu").toggleClass("show-on-click");
+	$(".nav-menu").toggleClass("show-on-click");       //while in mobile view this toggle the menu display
 })
