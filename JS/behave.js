acd3c265f28471facef9d7ba1d2ee1f554e04b71
Manav <manavchhabraj7@gmail.com>
2020-05-29 10:31:24 +0530

    All the elements added

--- JS/behave.js
+++ JS/behave.js
@@ -2,3 +2,7 @@ $(".menu-button").on("click",function ()
 {
 	$(".nav-menu").toggleClass("show-on-click");       //while in mobile view this toggle the menu display
 })
+$(".quick-links-button").on("click",function () 
+{
+	$(".quick-links").toggleClass("display-on-click");       //while in mobile view this toggle the menu display
+})
