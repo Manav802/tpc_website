227f9d3b18074e92fc28285b2246300268856398
Manav <manavchhabraj7@gmail.com>
2020-06-02 07:56:31 +0530

    Some changes to the footer

--- JS/behave.js
+++ JS/behave.js
@@ -4,5 +4,5 @@ $(".menu-button").on("click",function ()
 })
 $(".quick-links-button").on("click",function () 
 {
-	$(".quick-links").toggleClass("display-on-click");       //while in mobile view this toggle the menu display
+	$(".quick-links").toggleClass("display-on-click");       //while in mobile view this toggle the quicklinks display
 })
