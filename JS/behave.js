ed0444f515e997e072b7795723f9b2772ec2ae1e
Manav <manavchhabraj7@gmail.com>
2020-06-04 20:51:31 +0530

    Fixed the toggle issue QuickLinks

--- JS/behave.js
+++ JS/behave.js
@@ -1,18 +1,20 @@
 $(".menu-button").on("click",function () 
 {
-	$(".nav-menu").toggleClass("show-on-click");       //while in mobile view this toggle the menu display
+	$(".nav-menu").fadeToggle();      //while in mobile view this toggle the menu display
 })
+
 $(".quick-links-button").on("click",function () 
 {
-	$(".quick-links").toggleClass("display-on-click");       //while in mobile view this toggle the quicklinks display
+	$(".quick-links").toggleClass("hide-if-small-screen");    //while in mobile view this toggle the quicklinks display
 })
 
 $("#quick-links ").on("mouseleave",function ()
 {
-	setTimeout(function(){$(".quick-links").removeClass("display-on-click");}, 1000);
+	$(".quick-links").addClass("hide-if-small-screen");	
 })
+
 
 $("#menu-and-logo").on("mouseleave",function ()
 {
-	setTimeout(function(){ $(".nav-menu").addClass("show-on-click"); }, 1000);
+	$(".nav-menu").fadeOut(); 
 })
