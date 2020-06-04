459b81916470e3d1bc9aa07c9326974a1236f033
Manav <manavchhabraj7@gmail.com>
2020-06-04 16:23:47 +0530

    Final Layout ready

--- JS/behave.js
+++ JS/behave.js
@@ -6,3 +6,13 @@ $(".quick-links-button").on("click",function ()
 {
 	$(".quick-links").toggleClass("display-on-click");       //while in mobile view this toggle the quicklinks display
 })
+
+$("#quick-links ").on("mouseleave",function ()
+{
+	setTimeout(function(){$(".quick-links").removeClass("display-on-click");}, 1000);
+})
+
+$("#menu-and-logo").on("mouseleave",function ()
+{
+	setTimeout(function(){ $(".nav-menu").addClass("show-on-click"); }, 1000);
+})
